f1a3c9a756ed636ce90d5421e6f14d946bc8a016
Bocian117 <54430214+Bocian117@users.noreply.github.com>
2021-05-02 19:37:12 +0200

    2 raport

---
+++ KsiekowoscMobile/app/src/main/java/com/example/ksiekowoscmobile/ui/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ksiekowoscmobile.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ksiekowoscmobile.R
+
+class HomeActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+    }
+}
